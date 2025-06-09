317ac9de3c8aa45535f20cc6f6a9523904afca9f
Shubhra Sharma <shubhra187btcseai23@igdtuw.ac.in>
2025-06-09 16:30:56 +0530

    Added user and video models and utils folders

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
-
-
-
-connectDB();
-// import express from "express";
-
-// const app=express();
-// ( async () => {
-//     try{
-//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//        app.on("error", (error) => {
-//         console.log("ERROR:", error);
-//         throw error;
-//        })
-//        app.listen(process.env.PORT,() =>{
-//         console.log(`App is listening on port ${process.env.PORT}`);
-//        })
-//     }
-//     catch(error){
-//         console.error("ERROR: ", error);
-//         throw err;
-//     }
-// })()
+import {app} from './app.js';
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running on port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+  console.log('MongoDb connection failed!', err);
+})
